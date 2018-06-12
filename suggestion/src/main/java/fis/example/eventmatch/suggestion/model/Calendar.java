eb68b16110526875b4eea0a8d8df12a73b459440
David Gordon <davgordo@redhat.com>
2018-06-12 17:28:51 -0400

    implement suggestion service consuming calendar service API

---
+++ suggestion/src/main/java/fis/example/eventmatch/suggestion/model/Calendar.java
@@ -0,0 +1,39 @@
+package fis.example.eventmatch.suggestion.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calendar {
+
+    @JsonProperty
+    String userId;
+
+    @JsonProperty
+    List<Entry> entryList = new ArrayList<Entry>();
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public List<Entry> getEntryList() {
+        return entryList;
+    }
+
+    public void setEntryList(List<Entry> entryList) {
+        this.entryList = entryList;
+    }
+
+    @Override
+    public String toString() {
+        return "Calendar{" +
+                "userId='" + userId + '\'' +
+                ", entryList=" + entryList +
+                '}';
+    }
+}
--- suggestion/src/test/java/fis/example/eventmatch/suggestion/ApplicationTest.java
+++ suggestion/src/test/java/fis/example/eventmatch/suggestion/ApplicationTest.java
@@ -1,6 +1,9 @@
 package fis.example.eventmatch.suggestion;
 
+import org.apache.camel.EndpointInject;
+import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.apache.camel.test.spring.MockEndpointsAndSkip;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,20 +14,32 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import static org.apache.camel.builder.Builder.constant;
+
 @RunWith(CamelSpringBootRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@MockEndpointsAndSkip("rest:*")
 public class ApplicationTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
-    @Test
-    public void testGetSuggestionsNoAvailability() throws Exception {
+    @EndpointInject(uri = "mock:rest:get:calendars/{userId}")
+    MockEndpoint mockCalendarService;
 
-        String suggestionList = "[]";
-        ResponseEntity<String> response = restTemplate.getForEntity("/suggestions?user=davgordo", String.class);
+    @Test
+    public void testGetUserCalendar() throws Exception {
+
+        String mockCalendarResponse = "{\"userId\":\"davgordo\",\"entryList\":[{\"available\":true,\"start\":1535819400000,\"end\":1535828400000}]}";
+        String expectedSuggestionList = "[{\"score\":0,\"event\":\"Perfect fit for davgordo\"}]";
+        mockCalendarService.returnReplyBody(constant(mockCalendarResponse));
+        mockCalendarService.expectedMessageCount(1);
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/suggestions?userId=davgordo", String.class);
+
+        mockCalendarService.assertIsSatisfied();
         Assert.assertEquals(HttpStatus.OK,response.getStatusCode());
-        Assert.assertEquals(suggestionList, response.getBody());
+        Assert.assertEquals(expectedSuggestionList, response.getBody());
 
     }
 }
